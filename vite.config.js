68f14df0f87ffd33f7e184882577df09e1dfcb24
Your Name <you@example.com>
2025-06-15 21:35:00 +0100

    Ajout fichieviteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',      // Important for proper asset loading on Netlify
   plugins: [react()],
 })
