a7d1510cbe84080872cb9c217da3b478ed9ac8c1
Chammar_Oussama <165934219+CHOUMR11@users.noreply.github.com>
2025-06-15 22:55:34 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',          // Use relative base path for correct asset loading on static hosting
   plugins: [react()],
   build: {
-    outDir: 'dist',    // Output directory for build (default is 'dist')
-    sourcemap: false,  // Optional: disable sourcemaps for production build to reduce size
+    outDir: 'dist',
   },
-})
+  server: {
+    port: 3000,
+  }
+});
